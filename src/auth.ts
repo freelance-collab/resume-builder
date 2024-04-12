af88257ef30d5dbccd997054a1eb50075acba046
Elalfy74 <mahmoudelalfy13@gmail.com>
2024-04-12 19:47:39 +0200

    modified authed user obj

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,16 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import NextAuth from 'next-auth';
+import NextAuth, { type DefaultSession } from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 
 import prisma from './lib/prisma';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession['user'];
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -16,6 +24,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
     authorized: ({ auth }) => {
       return !!auth;
+    },
+    jwt: async ({ token, user }) => {
+      if (user) token.id = user.id;
+
+      return token;
+    },
+    session: async ({ session, token, user }) => {
+      if (token.id) session.user.id = token.id as string;
+
+      return session;
     },
   },
 });
