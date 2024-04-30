080635c0a13e59788b49d57c309fbcbcb5739f6a
mohamedsalem401 <mohamedsalem401@yahoo.com>
2024-04-30 12:58:40 +0300

    added google auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,40 +1,83 @@
 'use client';
 
+import { AlertCircle } from 'lucide-react';
 import { useSearchParams } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function LoginPage() {
   const searchParams = useSearchParams();
+  const error = searchParams.get('error');
+  const callbackUrl = searchParams.get('callback');
 
-  const handleSignIn = async () => {
-    await signIn('github', {
-      callbackUrl: searchParams.get('callback') ?? '/',
+  const handleSignIn = async (provider: 'github' | 'google') => {
+    await signIn(provider, {
+      callbackUrl: callbackUrl ?? '/',
     });
   };
 
   return (
-    <Card className='mx-auto mt-20 max-w-sm'>
-      <CardHeader>
-        <CardTitle className='text-2xl'>Login</CardTitle>
-        <CardDescription>Enter your email below to login to your account</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Button variant='outline' className='flex w-full items-center gap-2' onClick={handleSignIn}>
-          <svg
-            className='dark:fill-white'
-            xmlns='http://www.w3.org/2000/svg'
-            width='20'
-            height='20'
-            viewBox='0 0 30 30'
-          >
-            <path d='M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z'></path>
-          </svg>
-          Login with Github
-        </Button>
-      </CardContent>
-    </Card>
+    <div className='container max-w-lg'>
+      <Card className='mt-20'>
+        <CardHeader>
+          {error && (
+            <Alert variant='destructive' className='mb-2'>
+              <AlertCircle className='h-4 w-4' />
+              <AlertTitle>Error</AlertTitle>
+              <AlertDescription>
+                {error === 'OAuthAccountNotLinked' ? 'Email is already connected to other provider' : error}
+              </AlertDescription>
+            </Alert>
+          )}
+          <CardTitle className='text-2xl'>Login</CardTitle>
+          <CardDescription>Enter your email below to login to your account</CardDescription>
+        </CardHeader>
+        <CardContent className='space-y-4'>
+          <Button variant='outline' className='flex w-full items-center gap-2' onClick={() => handleSignIn('github')}>
+            <svg
+              className='dark:fill-white'
+              xmlns='http://www.w3.org/2000/svg'
+              width='20'
+              height='20'
+              viewBox='0 0 30 30'
+            >
+              <path d='M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z'></path>
+            </svg>
+            Login with Github
+          </Button>
+
+          <Button variant='outline' className='flex w-full items-center gap-2' onClick={() => handleSignIn('google')}>
+            <svg
+              width='20'
+              height='20'
+              viewBox='-3 0 262 262'
+              xmlns='http://www.w3.org/2000/svg'
+              preserveAspectRatio='xMidYMid'
+            >
+              <path
+                d='M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027'
+                fill='#4285F4'
+              />
+              <path
+                d='M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1'
+                fill='#34A853'
+              />
+              <path
+                d='M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782'
+                fill='#FBBC05'
+              />
+              <path
+                d='M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251'
+                fill='#EB4335'
+              />
+            </svg>
+            Login with Google
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth, { type DefaultSession } from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
 
 import prisma from './lib/prisma';
 
@@ -14,7 +15,7 @@ declare module 'next-auth' {
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub],
+  providers: [GitHub, Google],
   session: {
     strategy: 'jwt',
   },
